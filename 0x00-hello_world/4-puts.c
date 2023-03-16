7d5a5348907f5b5807d564ecbfbc79667fe4e85f
hamza-essalhi <hamzaessalhix@gmail.com>
2023-03-16 06:09:00 -0700

    Adding 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A function that return 0
+ * Return: 0 (Done)
+ */
+
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
