8bc2651dd97ae81015df1719a5e58447878492d0
hamza-essalhi <hamzaessalhix@gmail.com>
2023-03-16 07:33:42 -0700

    Adding the last file 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A function that print string not using printf or puts
+ * Return: 0 (Done)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout)
+}
