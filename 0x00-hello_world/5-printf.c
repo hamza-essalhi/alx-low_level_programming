11c9db353aa59982c186a890c6af644e49a52869
hamza-essalhi <hamzaessalhix@gmail.com>
2023-03-16 06:43:15 -0700

    Adding 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
-
 /**
- * main - A function that prin a sring 
- * Return: 0 (Done)
- */
-
+* main - A function that prin a sring
+* Return: 0 (Done)
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
