0f9bfae1e88284cd3914fe0e3f157a8ec43f48d2
hamza-essalhi <hamzaessalhix@gmail.com>
2023-03-17 09:06:30 -0700

    Adding 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  Prints the numbers from 0 to 9 separated by ,
+ * Return: 0 (Done)
+ */
+int main(void)
+{
+int n = 48; /* ascii code of number 0*/
+for (; n < 58 ; n++)
+{
+	putchar(n);
+	if (n < 57)
+	{
+		putchar(',')
+	}
+}
+printf("\n");
+return (0);
+}
