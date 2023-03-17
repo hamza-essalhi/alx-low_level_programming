d83f1095402d5824c7145b8f9a1ed52337920aa4
hamza-essalhi <hamzaessalhix@gmail.com>
2023-03-17 08:54:57 -0700

    Adding 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -  Prints the numbers of base 16
+ * Return: 0 (Done)
+ */
+int main(void)
+{
+int n = 48;
+char a = 'a';
+char f = 'f';
+for (; n <= 57 ; n++)
+{
+	putchar(n);
+	if (n == 57)
+	{
+		for (; a <= f ; a++)
+		{
+			putchar(a)
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
