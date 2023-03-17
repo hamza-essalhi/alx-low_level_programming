81064f2b22430d5d202f4f8dfb16ec86011402a6
hamza-essalhi <hamzaessalhix@gmail.com>
2023-03-17 08:24:51 -0700

    Adding 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -  Prints the numbers from 0 to 9
+ * Return: 0 (Done)
+ */
+int main(void)
+{
+int n = 0;
+for (; n < 10 ; n++)
+{
+	printf("%d", n);
+}
+printf("\n");
+return (0);
+}
