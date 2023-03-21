8c1d087c374f9eafb9fcd808e141c96db49710b2
hamza essalhi <42662723+hamza-essalhi@users.noreply.github.com>
2023-03-21 20:25:31 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,21 @@
-#include <stdio.h>
-/**
- * main -  _putchar
- * Return: 0 (Done)
- */
-int main(void)
-{
-	printf("_putchar")
-}
+#include "main.h"
+	/**
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
+
+
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
