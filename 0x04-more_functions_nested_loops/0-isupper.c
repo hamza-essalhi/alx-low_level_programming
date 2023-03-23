88ebe4d28b5dd0887d4625afd50c5a24ff7c624f
hamza-essalhi <hamzaessalhix@gmail.com>
2023-03-23 04:44:29 -0700

    Adding 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Get ASCII from A to Z.
+ * @c: int to check
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
+
