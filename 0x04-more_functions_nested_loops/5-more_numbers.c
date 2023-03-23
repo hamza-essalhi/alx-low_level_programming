e7156079a16e1cec02336d5108ec03ee77cfa37b
hamza-essalhi <hamzaessalhix@gmail.com>
2023-03-23 06:11:19 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prit numbers from 0 to 14 10 times.
+ * Return: void.
+ */
+void more_numbers(void)
+{
+char i;
+int j;
+char k;
+for (j = 0 ; j < 10 ; j++)
+{
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		_putchar(i);
+		if (i == '9')
+		{
+			for (k = '0'; k <= '4' ; k++)
+			{
+				_putchar('1');
+				_putchar(k);
+			}
+		}
+	}
+
+_putchar('\n');
+}
+_putchar('\n');
+}
