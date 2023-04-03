d93158893b9325a4d9f850dff813f622a031aa7e
hamza-essalhi <hamzaessalhix@gmail.com>
2023-04-03 13:40:09 -0700

    Adding tasks from 4 to 8 and Updating main.h file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
-/*taks 1*/
+/*taks 0*/
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
-/*task 2*/
+/*task 1*/
 char *_memcpy(char *dest, char *src, unsigned int n);
-/*task 3*/
+/*task 2*/
 char *_strchr(char *s, char c);
-/*task 4*/
+/*task 3*/
 unsigned int _strspn(char *s, char *accept);
+/*task 4*/
+char *_strpbrk(char *s, char *accept);
+/*task 5*/
+char *_strstr(char *haystack, char *needle);
+/*task 6*/
+void print_chessboard(char (*a)[8]);
+/*task 7*/
+void print_diagsums(int *a, int size);
+/*task 8*/
+/*task 9*/
 #endif
