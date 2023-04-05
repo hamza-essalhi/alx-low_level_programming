2718cb2eb2ec99acf6c9cb3fe4aa011e52bd9c2b
hamza-essalhi <hamzaessalhix@gmail.com>
2023-04-05 11:38:52 -0700

    Adding 0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+/**
+ * _puts_recursion - print a str
+ * @s: str
+ * Return:void
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*taks 0*/
+int _putchar(char c);
+void _puts_recursion(char *s);
+/*taks 1*/
+
+
+#endif
