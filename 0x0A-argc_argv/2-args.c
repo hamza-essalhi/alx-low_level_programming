4305a264984c16aec877103e0875cf80959dfab5
hamza-essalhi <hamzaessalhix@gmail.com>
2023-04-08 06:32:32 -0700

    Pushig task 2 : The best argument against democracy is a five-minute conversation with the average voter

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints all args
+ * @argc: Number of args
+ * @argv: Array of args
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
