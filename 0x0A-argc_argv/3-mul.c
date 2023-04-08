3bf22f1c57a02b9f26b492450a77ffab8f5646e8
hamza-essalhi <hamzaessalhix@gmail.com>
2023-04-08 06:56:41 -0700

    Pushing task 3 :Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,15 +7,7 @@
  */
 int _atoi(char *s)
 {
-
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	int i = 0, d = 0, n = 0, len = 0, f = 0, digit = 0;
 
 	while (s[len] != '\0')
 	{
@@ -29,7 +21,6 @@ int _atoi(char *s)
 		}
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-
 			digit = s[i] - '0';
 			if (d % 2)
 			{
@@ -38,14 +29,12 @@ int _atoi(char *s)
 			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-		    	{
+
 				break;
-		    	}
 			f = 0;
 		}
 		i++;
 	}
-
 	if (f == 0)
 	{
 		return (0);
@@ -64,12 +53,10 @@ int main(int argc, char *argv[])
 	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
-
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
