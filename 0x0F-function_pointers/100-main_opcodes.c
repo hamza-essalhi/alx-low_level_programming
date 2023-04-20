161e780fce13f922011afb1672e47253874deca5
hamza-essalhi <hamzaessalhix@gmail.com>
2023-04-20 09:58:58 -0700

    done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main- Entry point
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Description: this program prints opcodes in hexa)?
+* Return: 0 in succes
+*/
+
+
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
+
