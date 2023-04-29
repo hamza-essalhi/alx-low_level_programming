6c110ef9c7bcd68eef707c726bdf32fd635f0b28
hamza-essalhi <hamzaessalhix@gmail.com>
2023-04-29 08:28:18 -0700

    Fixing task 0 errors

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,12 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+/*Code By Hamza essalhi*/
 
 /**
  * struct arr_strc - The linked list
- * @_int: Integer
- * @next_point:
+ * @n: Integer
+ * @next: Next point
  *
  * Description: Linked list that for node struct
  *
@@ -16,9 +16,9 @@
 
 typedef struct arr_strc
 {
-          int n;
-          struct arr_strc *next;
-}listint_t;
+	int n;
+	struct arr_strc *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
