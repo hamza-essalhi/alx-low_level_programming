1c13abe2c31b8782e7b8fb98c630f4f0de2bb143
hamza-essalhi <hamzaessalhix@gmail.com>
2023-04-30 12:14:25 -0700

    fex0

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -15,33 +15,30 @@ size_t free_listint_safe(listint_t **h)
 
 
 	if (!h || !*h)
-	{
 		return (0);
-	}
-	else
+
+	while (*h)
 	{
-		while (*h)
+		auth = *h - (*h)->next;
+		if (auth < 0)
 		{
-			auth = *h - (*h)->next;
-			if (auth < 0)
-			{
-				free(*h);
-				*h = NULL;
-				length++;
-				break;
-			}
-			else
-			{
-				buffer = (*h)->next;
-				free(*h);
-				*h = buffer;
-				length++;
-			}
+			free(*h);
+			*h = NULL;
+			length++;
+			break;
 		}
-
-		*h = NULL;
-
-		return (length);
+		else
+		{
+			buffer = (*h)->next;
+			free(*h);
+			*h = buffer;
+			length++;
+		}
 	}
+
+	*h = NULL;
+
+	return (length);
+
 }
 
