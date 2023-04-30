69251a5185dbcad50981cd335b11f5ca1122d852
hamza-essalhi <hamzaessalhix@gmail.com>
2023-04-30 12:20:27 -0700

    t

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,65 +5,60 @@ size_t get_list_len(const listint_t *head);
 size_t print_list_safe(const listint_t *head);
 
 /**
- * get_list_len - Counts the number of nodes.
- * @head: A pointer.
+ * get_list_len - Counts the number of nodes in a linked list.
+ * @head: A pointer to the head of the list.
  *
- * Return: The number of nodes.
+ * Return: The number of nodes in the list.
  */
 size_t get_list_len(const listint_t *head)
 {
-	const listint_t *t, *h;
+	const listint_t *tortoise, *hare;
 	size_t nodes = 0;
 
 	if (head == NULL)
-	{
 		return (0);
-	}
-	else
+
+	tortoise = head;
+	hare = head;
+
+	while (hare && hare->next)
 	{
-		h = head;
-		t = head;
-		
-		while (h && hare->next)
-		{
-			t = t->next;
-			h = h->next->next;
+		tortoise = tortoise->next;
+		hare = hare->next->next;
 
-			if (t == h)
+		if (tortoise == hare)
+		{
+			do {
+				nodes++;
+				hare = hare->next;
+			} while (tortoise != hare);
+
+			tortoise = head;
+			while (tortoise != hare)
 			{
-				while (t!= h)
-				{
-					nodes++;
-					h = h->next;
-				}
-
-				t = h;
-				while (t != h)
-				{
-					nodes++;
-					t = t->next;
-					h= h->next;
-				}
-
-				return (nodes);
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
 			}
-		}
 
-		while (head)
-		{
-			nodes++;
-			head = head->next;
+			return (nodes);
 		}
-
-		return (nodes);
 	}
+
+	while (head)
+	{
+		nodes++;
+		head = head->next;
+	}
+
+	return (nodes);
 }
 
 /**
- * print_list_safe - Prints a listint_t.
- * @head: A pointer.
+ * print_list_safe - Prints a listint_t linked list safely.
+ * @head: A pointer to the head of the listint_t list.
  *
- * Return: The number of nodes.
+ * Return: The number of nodes in the list.
  */
 size_t print_list_safe(const listint_t *head)
 {
