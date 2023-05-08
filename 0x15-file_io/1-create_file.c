ef34bbf3d35f229d6b094ad1904ee1dfedf57c56
hamza-essalhi <hamzaessalhix@gmail.com>
2023-05-08 07:15:30 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,20 +23,15 @@ int create_file(const char *filename, char *text_content)
 			for (length = 0; text_content[length]; length++);
 		}
 
-		else
-		{
-			file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-			bytes_written = write(file_descriptor, text_content, length);
+		file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		bytes_written = write(file_descriptor, text_content, length);
 
-			if (file_descriptor == -1 || bytes_written == -1)
-			{
-				return (-1);
-			}
-			close(file_descriptor);
+		if (file_descriptor == -1 || bytes_written == -1)
+			return (-1);
 
-			return (1);
-		}
+		close(file_descriptor);
+
+		return (1);
 	}
-	return(1);
 }
 
